fdbbe3cd5025ca79ab3bebb344f92d05fbe29f88
Veronica <vivettemoss@yahoo.com>
2023-12-20 17:36:46 -0600

    AddeUserLogOutbackendPoint

--- project/backend/routes/api/session.js
+++ project/backend/routes/api/session.js
@@ -7,7 +7,7 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
-// Log in
+//* Log in
 router.post("/", async (req, res, next) => {
   const { credential, password } = req.body;
 
@@ -40,6 +40,12 @@ router.post("/", async (req, res, next) => {
   return res.json({
     user: safeUser,
   });
+});
+
+//* Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
 });
 
 module.exports = router;

779f980ea1b43ab9e7c4827cafa3183f78fe448f
Veronica <vivettemoss@yahoo.com>
2023-12-19 18:21:37 -0600

    Phase2Complete

--- project/backend/routes/api/index.js
+++ project/backend/routes/api/index.js
@@ -5,4 +5,14 @@ router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
 
+/* fetch('/api/test', {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+  },
+  body: JSON.stringify({ hello: 'world' })
+}).then(res => res.json()).then(data => console.log(data)); */
+// example fetch request
+
 module.exports = router;

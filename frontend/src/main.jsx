ec37492e43c0d66786cc7c7d94812cd5299b3007
Veronica <vivettemoss@yahoo.com>
2024-02-15 16:18:06 -0600

    Phase1Completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 import "./index.css";
 import configureStore from "./store";
 
@@ -13,6 +14,7 @@ if (import.meta.env.MODE !== "production") {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(

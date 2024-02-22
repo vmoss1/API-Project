6dcd869414d63599eb4c2e123c0f17e17f827475
Veronica <vivettemoss@yahoo.com>
2024-02-22 12:35:55 -0600

    Addedoptionstobackend

--- backend/db/seeders/20240102181335-demo-group.js
+++ backend/db/seeders/20240102181335-demo-group.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const { Group } = require("../models");
-const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
--- backend/db/seeders/20240103002239-demo-event-img.js
+++ backend/db/seeders/20240103002239-demo-event-img.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const { Eventimage } = require('../models')
+const { Eventimage } = require("../models");
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -18,68 +18,71 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-    try { await Eventimage.bulkCreate([
-      {
-        eventId: 1,
-        url: '../../../images/kayaking-party.jpeg',
-        preview: true
-      },
-      {
-        eventId: 2,
-        url: '../../../images/safety.jpeg',
-        preview: true
-      },
-      {
-        eventId: 3,
-        url: '../../../images/camping-store.jpeg',
-        preview: true
-      },
-      {
-        eventId: 4,
-        url: 'GrpMountainsImg.jpeg',
-        preview: false
-      },
-      {
-        eventId: 5,
-        url: 'GrpPaintingImg.jpeg',
-        preview: true
-      },
-      {
-        eventId: 6,
-        url: 'GrpSwimmingImg',
-        preview: false
-      },
-      {
-        eventId: 7,
-        url: 'GrpBridgeImg.jpeg',
-        preview: true
-      },
-      {
-        eventId: 8,
-        url: 'GrpGhostImg.jpeg',
-        preview: false
-      },
-      {
-        eventId: 9,
-        url: 'GrpBeerImg.jpeg',
-        preview: true
-      },
-      {
-        eventId: 10,
-        url: 'GrpArtImg.jpeg',
-        preview: true
-      }
-     ] , { validate: true 
-    })
-     } catch (err) {
-      console.error(err)
-      throw new Error('Check Validators')
-     }
+     */
+    try {
+      await Eventimage.bulkCreate(
+        [
+          {
+            eventId: 1,
+            url: "../../../images/kayaking-party.jpeg",
+            preview: true,
+          },
+          {
+            eventId: 2,
+            url: "../../../images/safety.jpeg",
+            preview: true,
+          },
+          {
+            eventId: 3,
+            url: "../../../images/camping-store.jpeg",
+            preview: true,
+          },
+          {
+            eventId: 4,
+            url: "GrpMountainsImg.jpeg",
+            preview: false,
+          },
+          {
+            eventId: 5,
+            url: "GrpPaintingImg.jpeg",
+            preview: true,
+          },
+          {
+            eventId: 6,
+            url: "GrpSwimmingImg",
+            preview: false,
+          },
+          {
+            eventId: 7,
+            url: "GrpBridgeImg.jpeg",
+            preview: true,
+          },
+          {
+            eventId: 8,
+            url: "GrpGhostImg.jpeg",
+            preview: false,
+          },
+          {
+            eventId: 9,
+            url: "GrpBeerImg.jpeg",
+            preview: true,
+          },
+          {
+            eventId: 10,
+            url: "GrpArtImg.jpeg",
+            preview: true,
+          },
+        ],
+        { validate: true }
+      );
+    } catch (err) {
+      console.error(err);
+      throw new Error("Check Validators");
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = "Eventimages"; 
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Eventimages";
     /**
      * Add commands to revert seed here.
      *
@@ -87,6 +90,6 @@ module.exports = {
      * await queryInterface.bulkDelete('People', null, {});
      */
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options,  null , {})
-  }
+    return queryInterface.bulkDelete(options, null, {});
+  },
 };
